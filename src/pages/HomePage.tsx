c9e38a6fbe65c5cdd3c909a6dea6366effb37bce
Dan Soutar <danielsoutar@gmail.com>
2023-04-17 13:28:39 -0400

    list blogs on home page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,10 +1,30 @@
-import type { Component } from 'solid-js'
-
+import { Component, createSignal, createEffect } from 'solid-js'
 import { useContext } from 'solid-js'
+
 import { GlobalContext } from '../GlobalContext'
+import { httpClient } from '../http'
 
 export const HomePage: Component = () => {
   const { authenticated }: any = useContext(GlobalContext)
 
-  return <div>HomePage - authenticated: {authenticated() ? 'Logged in' : 'Not logged in'}</div>
+  const [blogs, setBlogs]: any = createSignal()
+
+  createEffect(() => {
+    httpClient
+      .get('/blog', { params: { order_by: 'created_at:desc' } })
+      .then((response) => setBlogs(response.data.blogs))
+      .catch((error) => {
+        console.log(error)
+      })
+  })
+
+  return (
+    <div>
+      HomePage - authenticated: {authenticated() ? 'Logged in' : 'Not logged in'}
+      {blogs() &&
+        blogs().map((blog: any) => {
+          return <div>{blog.title}</div>
+        })}
+    </div>
+  )
 }
